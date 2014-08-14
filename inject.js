29dcdd9df671a50697e937a7b8803ee3b72645f2
Ciel <imwithye@gmail.com>
2014-08-14 16:52:35 +0800

    Get user info from url

--- inject.js
+++ inject.js
@@ -1,24 +1,31 @@
 (function() {
-  url = 'http://course-cal.appspot.com/perform.php?json=';
-  args = {
-      'mode' : 'auto',
-      'unique_id' : 'auto_mode',
-      'tz' : 'Asia/Singapore',
-      'filename': 'NTU Course Calendar',
-      'year' : '2014',
-      'sem' : '1',
-      'courses' : [
-      ]
-  };
-
-  table = document.getElementsByTagName('table')[2];
-  tr = table.getElementsByTagName('tr');
-  for(var i=1; i < tr.length-1; i++) {
-    index = tr[i].getElementsByTagName('td')[0]. getElementsByTagName('font')[0].innerHTML.trim();
-    code = tr[i].getElementsByTagName('td')[1]. getElementsByTagName('font')[0].innerHTML.trim();
-    examTime = tr[i].getElementsByTagName('td')[4]. getElementsByTagName('font')[0].innerHTML.trim();
-    args.courses.push({code: code, index: index});
+  var getInfo = function() {
+    var url = document.URL;
+    var args = url.split('?')[1].split('&');
+    var info = {};
+    info.mode = 'auto';
+    info.tz = 'Asia/Singapore',
+    info.unique_id = args[0].split('=')[1].trim();
+    info.year = args[3].split('=')[1].trim();
+    info.sem = args[4].split('=')[1].trim();
+    info.filename = 'AY-' + info.year + '-Sem-' + info.sem;
+    return info;
   }
-  url = url + JSON.stringify(args);
-  window.location = url;
+
+  var serverRequert = function() {
+    var url = 'http://course-cal.appspot.com/perform.php?json=';
+    var args = getInfo();
+    args.courses = Array();
+
+    var table = document.getElementsByTagName('table')[2];
+    var tr = table.getElementsByTagName('tr');
+    for(var i=1; i < tr.length-1; i++) {
+      var index = tr[i].getElementsByTagName('td')[0]. getElementsByTagName('font')[0].innerHTML.trim();
+      var code = tr[i].getElementsByTagName('td')[1]. getElementsByTagName('font')[0].innerHTML.trim();
+      var examTime = tr[i].getElementsByTagName('td')[4]. getElementsByTagName('font')[0].innerHTML.trim();
+      args.courses.push({code: code, index: index});
+    }
+    url = url + JSON.stringify(args);
+    window.location = url;
+  }();
 })();
